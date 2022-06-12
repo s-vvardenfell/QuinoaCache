39e16010e724dab29b03ba0d6c313f324d64e8b2
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-06-12 19:23:13 +0300

    removed redudand nesting

--- redis_cache/redis_cache_service.go
+++ redis_cache/redis_cache_service.go
@@ -2,23 +2,35 @@ package redis_cache
 
 import (
 	"context"
+	"fmt"
 	"time"
 
+	"github.com/go-redis/redis/v9"
 	"github.com/s-vvardenfell/Adipiscing/generated"
 )
 
 type Server struct {
 	generated.UnimplementedUserServiceServer
-	Rc *RedisCache
+	client *redis.Client
+}
+
+func NewServer(host, port, passw string, db int) *Server {
+	return &Server{
+		client: redis.NewClient(&redis.Options{
+			Addr:     fmt.Sprintf("%s:%s", host, port), //:6379
+			Password: passw,
+			DB:       db,
+		}),
+	}
 }
 
 func (s *Server) Get(ctx context.Context, in *generated.Key) (*generated.Value, error) {
-	res, err := s.Rc.get(in.Key)
+	res, err := s.client.Get(ctx, in.Key).Result()
 	return &generated.Value{Val: res}, err
 }
 
 func (s *Server) Set(ctx context.Context, in *generated.Input) (*generated.Ok, error) {
-	err := s.Rc.set(in.Key, in.Val, time.Duration(in.Exp))
+	err := s.client.Set(ctx, in.Key, in.Val, time.Duration(in.Exp)*time.Second).Err()
 	if err != nil {
 		return &generated.Ok{Ok: false}, err
 	}
