20eb270970dd5c07bc054689939a3d05ca2628fc
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-06-12 19:23:49 +0300

    tests for redis_cache pkg with client stub

--- redis_cache/redis_cache_test.go
+++ redis_cache/redis_cache_test.go
@@ -1,49 +1,99 @@
 package redis_cache
 
 import (
+	"context"
+	"log"
 	"math/rand"
+	"net"
 	"strconv"
 	"testing"
 	"time"
 
+	"github.com/s-vvardenfell/Adipiscing/generated"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
 )
 
+const (
+	redisPort = "6379"
+	servePort = ":50051"
+	host      = "localhost"
+)
+
+func RunServer() {
+	grpcServ := grpc.NewServer()
+	rcs := NewServer("localhost", redisPort, "", 0)
+	generated.RegisterUserServiceServer(grpcServ, rcs)
+
+	lis, err := net.Listen("tcp", servePort)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Printf("Starting gRPC listener on port " + servePort)
+	if err := grpcServ.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
 func Test_pckg(t *testing.T) {
-	rand.Seed(time.Now().UnixNano())
-	key := strconv.Itoa(rand.Intn(1000))
-	val1 := strconv.Itoa(rand.Intn(1000))
-	val2 := strconv.Itoa(rand.Intn(1000))
-	var rc *RedisCache
-	var res string
 
-	t.Log("\tCreating client")
+	go RunServer()
+
+	time.Sleep(2 * time.Second)
+
+	var key, val string
+	t.Log("\tGenerating random key and value")
 	{
-		rc = New("localhost", "6379", "", 0)
+		rand.Seed(time.Now().UnixNano())
+		key = strconv.Itoa(rand.Intn(1000))
+		val = strconv.Itoa(rand.Intn(1000))
 	}
 
-	t.Log("\tSetting value")
+	s := NewClientStub(host, servePort)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	t.Log("\tSet value")
 	{
-		rc.Create(key, val1)
+		res, err := s.c.Set(ctx, &generated.Input{Key: key, Val: val, Exp: 0})
+		require.NoError(t, err)
+		require.Equal(t, res.Ok, true)
 	}
 
-	t.Log("\tGetting value")
+	t.Log("\tGet value")
 	{
-		res = rc.Read(key)
-		require.Equal(t, val1, res)
+		res, err := s.c.Get(ctx, &generated.Key{Key: key})
+		require.NoError(t, err)
+		require.Equal(t, res.Val, val)
 	}
 
-	t.Log("\tUpdating value")
+	t.Log("\tGenerating new random key and value")
 	{
-		rc.Update(key, val2)
-		res = rc.Read(key)
-		require.NotEqual(t, val1, res)
+		rand.Seed(time.Now().UnixNano())
+		key = strconv.Itoa(rand.Intn(1000))
+		val = strconv.Itoa(rand.Intn(1000))
 	}
 
-	t.Log("\tDeleting value")
+	t.Log("\tSet value with expire time 5 seconds")
 	{
-		rc.Delete(key)
-		res = rc.Read(key)
-		require.Empty(t, res)
+		res, err := s.c.Set(ctx, &generated.Input{Key: key, Val: val, Exp: 5})
+		require.NoError(t, err)
+		require.Equal(t, res.Ok, true)
+	}
+
+	t.Log("\tGet value with expire time after 1 second while it exists")
+	{
+		time.Sleep(1 * time.Second)
+		res, err := s.c.Get(ctx, &generated.Key{Key: key})
+		require.NoError(t, err)
+		require.Equal(t, res.Val, val)
+	}
+
+	t.Log("\tGet value with expire time after 5 seconds when it does not exists")
+	{
+		time.Sleep(4 * time.Second)
+		_, err := s.c.Get(ctx, &generated.Key{Key: key})
+		require.Error(t, err)
 	}
 }
