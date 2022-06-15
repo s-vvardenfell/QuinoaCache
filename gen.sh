c2f178c12874798c11c518410ebec36995269546
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-06-15 17:45:27 +0300

    update ci

--- gen.sh
+++ gen.sh
@@ -2,4 +2,5 @@
 protoc --go-grpc_out=. proto/service.proto
 protoc --go_out=. proto/service.proto
 
+#protoc -I. --go_opt=paths=source_relative --go_out=./generated proto/service.proto
 #protoc -I . proto/service.proto --go_out=. --go-grpc_out=. proto/service.proto
