a1dd6bda7f7b575e117b8786e6726772f2909f41
s.vvardenfell <s.vvardenfell@yandex.ru>
2022-06-15 16:33:48 +0300

    update ci

--- gen.sh
+++ gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-$GOPATH/bin/protoc --go-grpc_out=. proto/service.proto
-$GOPATH/bin/protoc --go_out=. proto/service.proto
+protoc --go-grpc_out=. proto/service.proto
+protoc --go_out=. proto/service.proto
 
 #protoc -I . proto/service.proto --go_out=. --go-grpc_out=. proto/service.proto
